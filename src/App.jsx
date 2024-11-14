142683826b29ed77446b718750eeb7ff5c4c81b2
Aktan <motivationaktan@gmail.com>
2024-11-14 17:17:21 +0600

    fdf

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/header/Header"
+import Hero from "./components/hero/Hero"
+import Welcome from "./components/welcome/Welcome"
+import './app.css'
+// component -App (JS , JSX , CSS)
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Welcome/>
+    </div>
+  )
+}
+
+export default App
